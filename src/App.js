50f5daa7197f1b5aab188a8fcc8e71a500de0ff6
tracybrtn <tracynburton@gmail.com>
2022-11-21 18:09:29 -0600

    solved react-dom issue

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import About from './components/about';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
