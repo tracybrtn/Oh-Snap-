028e739199780f62fd74637715a281f7c237ec5e
tracybrtn <tracynburton@gmail.com>
2022-11-25 12:21:35 -0600

    passing test for contact function

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -36,7 +36,7 @@ function ContactForm() {
 
   return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
@@ -55,7 +55,7 @@ function ContactForm() {
               <p className="error-text">{errorMessage}</p>
             </div>
           )}
-        <button type="submit">Submit</button>
+        <button type="submit" data-testid="button">Submit</button>
       </form>
     </section>
   )
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -5,10 +5,24 @@ import Nav from '..';
 
 afterEach(cleanup);
 
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
+
 describe('Nav component', () => {
     // baseline test
     it('renders', () => {
-      render(<Nav/>);
+      render(<Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+      />);
     });
  
   // snapshot test
@@ -17,7 +31,7 @@ describe('Nav component', () => {
 
       expect(asFragment()).toMatchSnapshot();
     });
-  })
+})
 
   describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
